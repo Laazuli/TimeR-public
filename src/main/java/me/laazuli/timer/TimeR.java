ac59a5b2f29170b1f27c4eb17831b0fecde31c16
laazuli <laazuli.laazuli@gmail.com>
2025-11-08 04:02:19 +0100

    discarded src/main/java/.../display since it overcomplicated things, renamed AddTimerLayer to AddTimerRenderLayer

--- src/main/java/me/laazuli/timer/TimeR.java
+++ src/main/java/me/laazuli/timer/TimeR.java
@@ -1,25 +1,36 @@
 package me.laazuli.timer;
 
 import me.laazuli.timer.command.TimerCommand;
-import me.laazuli.timer.display.ActionBarDisplay;
-import me.laazuli.timer.display.Display;
-import me.laazuli.timer.display.StringWidgetDisplay;
 import me.laazuli.timer.time.SystemTimeTimer;
 import me.laazuli.timer.time.Timer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.network.chat.Component;
 
 public class TimeR implements ClientModInitializer {
     public static final String MOD_ID = "timer";
 
     public static final Timer TIMER = new SystemTimeTimer();
 
-    public static final Display DISPLAY = new StringWidgetDisplay(TIMER);
+    public static AbstractWidget DISPLAY;
+//    public static final TimerDisplay DISPLAY = new StringWidgetTimerDisplay(TIMER);
 //    public static final Display DISPLAY = new ActionBarDisplay(TIMER);
 
     public static final TimerCommand TIMER_COMMAND = new TimerCommand(TIMER);
 
     @Override
     public void onInitializeClient() {
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            DISPLAY = new StringWidget(Component.literal("IF YOU SEE THIS, PLEASE CONTACT THE DEVELOPER!!"), Minecraft.getInstance().font);
+        });
+
         TIMER_COMMAND.register();
+    }
+
+    public static String formatTimer(Timer timer) {
+        return String.format("%02d:%02d:%02d", timer.getHours(), timer.getMinutes()%60 , timer.getSeconds()%60);
     }
 }
--- src/main/java/me/laazuli/timer/mixin/AddTimerRenderLayer.java
+++ src/main/java/me/laazuli/timer/mixin/AddTimerRenderLayer.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
-public class AddTimerLayer {
+public class AddTimerRenderLayer {
     @Shadow @Final private LayeredDraw layers;
 
     @Shadow @Final private Minecraft minecraft;
@@ -23,8 +24,13 @@ public class AddTimerLayer {
     @Inject(method = "<init>", at = @At("TAIL"))
     private void addTimerLayer(CallbackInfo ci) {
         LayeredDraw timerLayer = new LayeredDraw();
-        timerLayer.add((TimeR.DISPLAY::render));
+
+        timerLayer.add((guiGraphics, deltaTracker) -> {
+            TimeR.DISPLAY.setMessage(Component.literal(TimeR.formatTimer(TimeR.TIMER)));
+            TimeR.DISPLAY.render(guiGraphics, -1, -1, deltaTracker.getRealtimeDeltaTicks());
+        });
+
         this.layers.add(timerLayer, () -> !this.minecraft.options.hideGui && !this.debugOverlay.showDebugScreen() /* && !this.minecraft.player.isReducedDebugInfo() && !this.minecraft.options.reducedDebugInfo().get().booleanValue() */);
-        System.out.println("timer layer mixin");
+//        System.out.println("timer layer mixin");
     }
 }
